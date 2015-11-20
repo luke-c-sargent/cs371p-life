43fa55393cfa7086abdc9aaee05b70374909c228
Katie Taylor <katie.taylor@utexas.edu>
2015-11-19 21:21:07 -0600

    fixing IO

--- Life.h
+++ Life.h
@@ -134,7 +134,6 @@ class Life{
     getline(input_stream, line);
     frequency=stoi(line);
    
-    cout << rows << "x" << cols << " ***" << endl << endl;
     generation = 0;
     population = 0;
   }
