669376af7d7d015f3a6eb025bf9cd9e7ea1bdde0
Luke <luke.c.sargent@gmail.com>
2015-11-19 21:59:10 -0600

    doxygen and log

---
+++ html/search/classes_3.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['life',['Life',['../classLife.html',1,'']]],
+  ['life_5fiterator',['Life_Iterator',['../classLife__Iterator.html',1,'']]],
+  ['locale',['Locale',['../structLocale.html',1,'']]]
+];
